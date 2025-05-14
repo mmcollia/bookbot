8cbeed96a69e4ee215fba4aac4ae35a369530934
mmcollia <mmcollia@gmail.com>
2025-05-14 20:15:26 +0200

    G: Added sys.argv instead of filepath in order to analyse more books

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import * 
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -11,13 +12,17 @@ def print_report(report_dict):
         print(f"{letter['char']}: {letter['num']}")
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzin book found at books/frankenstein.txt")
-    print("------------ Word Count -------------")
-    print(count_words(get_book_text("books/frankenstein.txt")))
-    print("------------ Character Count ------------")
-    extracted_dict = text_to_char(get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    extracted_dict = text_to_char(get_book_text(sys.argv[1]))
     report_dict = report(extracted_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzin book found at {sys.argv[1]}")
+    print("------------ Word Count -------------")
+    print(count_words(get_book_text(sys.argv[1])))
+    print("------------ Character Count ------------")
     print_report(report_dict)
     print("============= END ===============")
 
